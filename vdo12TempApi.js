e115d4ba67d082e21cbc034a85b4f48b04d8285d
shubhamstr <shubhamsutar5799@gmail.com>
2022-01-15 20:19:31 +0530

    12 weather api

--- vdo12TempApi.js
+++ vdo12TempApi.js
@@ -5,17 +5,13 @@ const app = express();
 const requests = require("requests");
 
 
-// console.log(path.join(__dirname, '../public/staticsite'));
 // middleware
-// const pathsite = path.join(__dirname, '../public/staticsite');
-const pathsite = path.join(__dirname, '../public');
-const templatepath = path.join(__dirname, '../template/views');
-const partialspath = path.join(__dirname, '../template/partials');
+const templatepath = path.join(__dirname, './weather/views');
+const partialspath = path.join(__dirname, './weather/partials');
 // set template engine
 app.set('view engine', 'hbs');
 app.set('views', templatepath);
 hbs.registerPartials(partialspath)
-app.use(express.static(pathsite));
 
 // template engine route
 app.get("/", (req, res) => {
@@ -52,17 +48,10 @@ app.get("/", (req, res) => {
     res.send("hello from the express")
 })
 
-// error page 404 
-app.get("*", (req, res) => {
-    res.render("404", {
-        err: "oops page doesn't exists"
-    });
-})
-
 app.listen(8000, () => {
     console.log("listening on 8000 port")
     console.log("http://localhost:8000")
 })
 
 
-// nodemon src/vdo5.js -e js,hbs
+// nodemon vdo12TempApi.js -e js,hbs
